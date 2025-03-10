8eb2b9c182e1fe92323389e6886bf71f15b6c9bc
Alex Jonker <alex.jonker2008@gmail.com>
2025-03-10 12:54:27 +0100

    database in django

--- scootaloo/settings.py
+++ scootaloo/settings.py
@@ -79,13 +79,22 @@ WSGI_APPLICATION = 'scootaloo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'scootaloo',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': ''
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
